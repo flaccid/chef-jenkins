ca278fcf6d2572755cf8ec4c57ffbdec97d56b0b
Chris Fordham <chris@xhost.com.au>
2012-03-01 18:26:00 +1100

    Add outstanding attributes to metadata.

--- cookbooks/jenkins/metadata.rb
+++ cookbooks/jenkins/metadata.rb
@@ -15,6 +15,16 @@ attribute "jenkins/mirror",
   :required => "optional",
   :recipes => [ "jenkins::default" ]
   
+attribute "jenkins/java_home",
+  :display_name => "jenkins Java Home",
+  :description => "Java install path, used for for cli commands.",
+  :recipes => [ "jenkins::default" ]
+  
+attribute "jenkins/server/home",
+  :display_name => "jenkins Server Home",
+  :description => "JENKINS_HOME directory.",
+  :recipes => [ "jenkins::default" ]
+
 attribute "jenkins/server/user",
   :display_name => "jenkins Server User",
   :description => "User the Jenkins server runs as.",
@@ -33,15 +43,86 @@ attribute "jenkins/server/port",
   :default => '8080',
   :recipes => [ "jenkins::default" ]
 
-attribute "jenkins/server/plugins",
-  :display_name => "jenkins Server Plugins",
-  :description => "Download the latest version of plugins in this list, bypassing update center.",
-  :recipes => [ "jenkins::default" ]
+attribute "jenkins/server/url",
+  :display_name => "jenkins Server Port",
+  :description => " Base URL of the Jenkins server.",
+  :recipes => [ "jenkins::default" ]  
   
 attribute "jenkins/server/plugins",
   :display_name => "jenkins Server Plugins",
   :description => "Download the latest version of plugins in this list, bypassing update center.",
   :recipes => [ "jenkins::default" ]
+  
+attribute "jenkins/node/name",
+  :display_name => "jenkins Node Name",
+  :required => "optional",
+  :description => "Name of the node within Jenkins.",
+  :recipes => [ "jenkins::default" ]
+  
+attribute "jenkins/node/description",
+  :display_name => "jenkins Node Description",
+  :required => "optional",
+  :description => "Jenkins node description.",
+  :recipes => [ "jenkins::default" ]
+
+attribute "jenkins/node/executors",
+  :display_name => "jenkins Node Executors",
+  :default => "1",
+  :description => "Number of node executors.",
+  :recipes => [ "jenkins::default" ]
+
+attribute "jenkins/node/home",
+  :display_name => "jenkins Node Home",
+  :default => "/home/jenkins",
+  :description => "Home directory (“Remote FS root”) of the node.",
+  :recipes => [ "jenkins::default" ]
+
+attribute "jenkins/node/labels",
+  :display_name => "jenkins Node Labels",
+  :description => "Node labels.",
+  :recipes => [ "jenkins::default" ]
+
+attribute "jenkins/node/mode",
+  :display_name => "jenkins Node Mode",
+  :description => "Node usage mode, “normal” or “exclusive” (tied jobs only).",
+  :default => 'normal',
+  :recipes => [ "jenkins::default" ]
+  
+attribute "jenkins/node/launcher",
+  :display_name => "jenkins Launch Method",
+  :description => "Node launch method, “jnlp”, “ssh” or “command”.",
+  :default => 'ssh',
+  :recipes => [ "jenkins::default" ]
+
+attribute "jenkins/node/availability",
+  :display_name => "jenkins Node Availability",
+  :description => "“always” keeps node on-line, “demand” off-lines when idle.",
+  :default => 'always',
+  :recipes => [ "jenkins::default" ]
+
+attribute "jenkins/node/in_demand_delay",
+  :display_name => "jenkins In-Demand Delay",
+  :description => "number of minutes for which jobs must be waiting in the queue before attempting to launch this slave.",
+  :default => '0',
+  :recipes => [ "jenkins::default" ]
+
+attribute "jenkins/node/idle_delay",
+  :display_name => "jenkins Node Idle Delay",
+  :description => "number of minutes that this slave must remain idle before taking it off-line.",
+  :default => '1',
+  :recipes => [ "jenkins::default" ]
+
+attribute "jenkins/node/env",
+  :display_name => "jenkins Node Environment",
+  :description => "“Node Properties” -> “Environment Variables”.",
+  :default => nil,
+  :recipes => [ "jenkins::default" ]
+
+attribute "jenkins/node/user",
+  :display_name => "jenkins Node Environment",
+  :description => "user the slave runs as.",
+  :default => 'jenkins-node',
+  :recipes => [ "jenkins::default" ]  
   
 attribute "jenkins/node/ssh_host",
   :display_name => "jenkins Node SSH Host",
