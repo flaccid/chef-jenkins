4c9b6e6df3926ac62baad7e4bb2ed0a832376d9a
Chris Fordham <chris@xhost.com.au>
2012-03-01 15:43:40 +1100

    Add default recipe to metadata.

--- cookbooks/jenkins/metadata.rb
+++ cookbooks/jenkins/metadata.rb
@@ -5,3 +5,5 @@ description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.5"
 %w(runit java iptables).each { |cb| depends cb }
+
+recipe "jenkins::default", "Installs a Jenkins CI server using the jenkins-ci.org/redhat RPM. The recipe also generates an ssh private key and stores the ssh public key in the node ‘jenkins[:pubkey]’ attribute for use by the node recipes."
